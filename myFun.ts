b8de842455d5022bfe35d758a9cfac441f7f9e87
TomSssM <ilyashome3@gmail.com>
2021-12-10 16:45:34 +0300

    create fix

--- myFun.ts
+++ myFun.ts
@@ -3,12 +3,27 @@ class MyError extends Error {
   constructor(message: string) {
     super(message);
     this.SUPER_NAME = 'TOM';
+    Object.setPrototypeOf(this, MyError.prototype);
   }
 }
 
+class MyError2 extends MyError {
+    public SUPER_NAME2: string;
+    constructor(message: string) {
+        super(message);
+        this.SUPER_NAME2 = 'TOM2';
+        Object.setPrototypeOf(this, MyError2.prototype);
+    }
+}
+
 export function myFun() {
-    const e = new MyError('test');
+    const e = new MyError2('test');
+
+    console.log('e instanceof MyError');
     console.log(e instanceof MyError);
+
+    console.log('e instanceof MyError2');
+    console.log(e instanceof MyError2);
 }
 
 if (require.main === module) {
